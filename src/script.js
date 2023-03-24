fa413a69d1e91fa43bb37476850f720222bd9fc3
carolinesteen <32312963+carolinesteen@users.noreply.github.com>
2023-03-24 13:27:18 -0700

    Fixed @media resolution

--- src/script.js
+++ src/script.js
@@ -37,15 +37,7 @@ function getForecast(city) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
